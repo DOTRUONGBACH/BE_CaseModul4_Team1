398dfafd9c438ee7fc6278b47949979b2c2e7c0b
DuyHung02 <duyhung23102002@gmail.com>
2023-02-15 09:26:20 +0700

    upRegister

--- src/main/java/com/backend/controller/AccountController.java
+++ src/main/java/com/backend/controller/AccountController.java
@@ -3,8 +3,12 @@ package com.backend.controller;
 import com.backend.model.Account;
 import com.backend.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,12 +24,24 @@ public class AccountController {
     }
 
     @PostMapping
-    public Account save(Account account) {
+    public Account save(@RequestBody Account account) {
         return accountService.save(account);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable long id) {
         accountService.delete(id);
+    }
+
+    @PostMapping("/upImg")
+    public String upImg(@RequestParam MultipartFile fileImg) {
+        String nameImg = fileImg.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(fileImg.getBytes(), new File("D:\\CODEGYM\\Modul_4\\BaiTap\\CaseStudy_Modul4\\FE_CaseModul4_Team1\\images\\avatar\\" + nameImg));
+            return "/avatar/" + nameImg;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
